626c19e11c76cd21815a12ef0e0bfbb02e329963
PW <pw1016@qq.com>
2017-11-08 00:02:00 +0800

    goodnight

--- python/src/gensimTool/mygensim.py
+++ python/src/gensimTool/mygensim.py
@@ -1,39 +1,40 @@
+import logging
 import os
+from collections import defaultdict
 
 import jieba
 from gensim import corpora, models, similarities
+from gensim.corpora import Dictionary
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+"""
+激活日志
+Gensim使用Python标准的日志类来记录不同优先级的各种事件
+"""
 
 dict_file = 'D:/dict.txt'
-APPEND = 'APPEND'
-CREATE = 'CREATE'
+corpus_file = 'D:/corpus.mm'
+stop_dic_file = 'D:/stop_words.dic'
 
 
-def check(text, corpora_documents=None):
+def check(doc):
     """
     检查文档相似性
     :param text: 要检查的文档
-    :param corpora_documents: 语料库，如果不给定，则加载语料库进行检查
     :return:
     """
-    if text is None:
-        print("doc is blank")
+    if doc is None:
+        logging.error("待检测文档为空")
+        raise RuntimeError("待检测文档为空")
+    if not os.path.exists(corpus_file):
+        logging.warning("语料库不存在")
         return
-
-    # '''
-    # 生成字典和向量语料
-    # '''
-    if corpora_documents is not None:
-        dictionary = corpora.Dictionary(corpora_documents)
-        # dictionary.save_as_text(dict_file)
-    else:
-        if not os.path.exists(dict_file):
-            print("语料库不存在")
-            return
-        dictionary = corpora.Dictionary.load_from_text(dict_file)
-
+    # dictionary = corpora.Dictionary.load_from_text(dict_file)
     # 每一篇文档对应的稀疏向量（这里是bow向量）
-    corpus = [dictionary.doc2bow(doc) for doc in corpora_documents]
+    # corpus = [dictionary.doc2bow(doc) for doc in corpora_documents]
 
+    corpus = corpora.MmCorpus(corpus_file)
+    dictionary = Dictionary.from_corpus(corpus);
     tfidf_model = models.TfidfModel(corpus)
     '''
     TF-IDF是一种统计方法，用以评估一字词对于一个文件集或一个语料库中的其中一份文件的重要程度。
@@ -42,8 +43,17 @@ def check(text, corpora_documents=None):
     corpus_tfidf = tfidf_model[corpus]
 
     # 处理测试文本
-    text_corpus = dictionary.doc2bow(text)  # 转换成bow向量
+    text_corpus = dictionary.doc2bow(cut(doc))  # 转换成bow向量
     text_corpus_tfidf = tfidf_model[text_corpus]  # 计算tfidf值
+
+    """
+    similarities.MatrixSimilarity类仅仅适合能将所有的向量都在内存中的情况。
+    例如，如果一个百万文档级的语料库使用该类，可能需要2G内存与256维LSI空间。 
+    如果没有足够的内存，你可以使用similarities.Similarity类。该类的操作只
+    需要固定大小的内存，因为他将索引切分为多个文件（称为碎片）存储到硬盘上了。
+    它实际上使用了similarities.MatrixSimilarity和similarities.SparseMatrixSimilarity两个类，
+    因此它也是比较快的，虽然看起来更加复杂了。
+    """
 
     similarity_tfidf = similarities.Similarity(None, corpus_tfidf, num_features=600, num_best=5)
     result1 = similarity_tfidf[text_corpus_tfidf]
@@ -65,35 +75,75 @@ def train(doc):
     :param doc: 需要训练的文档
     :return:
     """
-    doc = [list(jieba.cut(doc))]
-    if os.path.exists(dict_file):
-        dictionary = corpora.Dictionary.load_from_text(dict_file)
-        dictionary.add_documents(doc)
+    # 分词
+    terms = cut(doc)
+
+    # 去停止词
+    stoplist = load_stop_words(stop_dic_file)
+    texts = [term for term in terms if term not in stoplist]
+
+    # texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+    texts = [texts]
+    print(texts)
+
+    # # 去掉只出现一次的词
+    # frequency = defaultdict(int)
+    # for text in texts:
+    #     for token in text:
+    #         frequency[token] += 1
+    # texts = [[token for token in text if frequency[token] > 1] for text in texts]
+
+    if os.path.exists(corpus_file):
+        # dictionary = corpora.Dictionary.load_from_text(dict_file)
+        mmcorpus = corpora.MmCorpus(corpus_file)
+        corpus = list(mmcorpus)
+        dictionary = Dictionary.from_corpus(corpus)
+        corpus.append(dictionary.doc2bow(terms))
     else:
-        dictionary = corpora.Dictionary(doc)
-    dictionary.save_as_text(dict_file)
+        dictionary = corpora.Dictionary(texts)
+        corpus = [dictionary.doc2bow(text) for text in texts]
+    corpora.MmCorpus.serialize(corpus_file, corpus)
+    print(list(corpus))
 
 
-def save_as_text(dictionary):
-    """
-    保存语料库
-    :param dictionary:
-    :return:
-    """
-    dictionary.save_as_text(dict_file)
 
 
-def add_documents(dictionary, documents, prune_at=None):
-    dictionary.add_documents(documents, prune_at)
+def train1(doc):
+    # 分词
+    documents = cut(doc)
+
+    # 去停止词
+    stoplist = load_stop_words(stop_dic_file)
+    texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+
+    # 去掉只出现一次的词
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+    texts = [[token for token in text if frequency[token] > 1] for text in texts]
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save(dict_file)
+
+
+def load_stop_words(file):
+    try:
+        with open(file) as f:
+            return set([line.strip().lower() for line in f.readlines()])
+    except Exception as e:
+        logging.error(e)
+        return []
 
 
 def cut(line):
-    return list(jieba.cut(line))
+    terms = list(jieba.cut(line))
+    return terms
 
 
 if __name__ == '__main__':
     raw_documents = [
-        '0南京江心洲污泥偷排”等污泥偷排或处置不当而造成的污染问题，不断被媒体曝光',
+        '0南京江心洲污泥偷排或处置不当而造成的污染问题，不断被媒体曝光',
         '1面对美国金融危机冲击与国内经济增速下滑形势，中国政府在2008年11月初快速推出“4万亿”投资十项措施',
         '2全国大面积出现的雾霾，使解决我国环境质量恶化问题的紧迫性得到全社会的广泛关注',
         '3大约是1962年的夏天吧，潘文突然出现在我们居住的安宁巷中，她旁边走着40号王孃孃家的大儿子，一看就知道，他们是一对恋人。那时候，潘文梳着一条长长的独辫',
@@ -111,6 +161,7 @@ if __name__ == '__main__':
         '15中共中央总书记、国家主席、中央军委主席习近平看望出席全国政协十二届二次会议的委员并参加分组讨论时强调，团结稳定是福，分裂动乱是祸。全国各族人民都要珍惜民族大团结的政治局面，都要坚决反对一切危害各民族大团结的言行'
     ]
 
+
     for item_text in raw_documents:
         train(item_text)
-    check(cut("南京江心洲污泥偷排”等污泥偷排或处置不当而造成的污染问题，不断被媒体曝光"))
+    # check("南京江心洲污泥偷排”等污泥偷排或处置不当而造成的污染问题，不断被媒体曝光")
