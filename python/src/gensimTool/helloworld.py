d3e2dad5c5e4af4347f41d3da2ef5ada9dc62d54
PW <pw1016@qq.com>
2017-11-07 18:23:57 +0800

    gensim预研

---
+++ python/src/gensimTool/helloworld.py
@@ -0,0 +1,15 @@
+from gensim.corpora import Dictionary
+
+texts = [['human', 'interface', 'computer'],
+         ['survey', 'user', 'computer', 'system', 'response', 'time'],
+         ['eps', 'user', 'interface', 'system'],
+         ['system', 'human', 'system', 'eps'],
+         ['user', 'response', 'time'],
+         ['trees'],
+         ['graph', 'trees'],
+         ['graph', 'minors', 'trees'],
+         ['graph', 'minors', 'survey']]
+
+dictionary = Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+print(corpus[0])
---
+++ python/src/gensimTool/mygensim.py
@@ -0,0 +1,116 @@
+import os
+
+import jieba
+from gensim import corpora, models, similarities
+
+dict_file = 'D:/dict.txt'
+APPEND = 'APPEND'
+CREATE = 'CREATE'
+
+
+def check(text, corpora_documents=None):
+    """
+    检查文档相似性
+    :param text: 要检查的文档
+    :param corpora_documents: 语料库，如果不给定，则加载语料库进行检查
+    :return:
+    """
+    if text is None:
+        print("doc is blank")
+        return
+
+    # '''
+    # 生成字典和向量语料
+    # '''
+    if corpora_documents is not None:
+        dictionary = corpora.Dictionary(corpora_documents)
+        # dictionary.save_as_text(dict_file)
+    else:
+        if not os.path.exists(dict_file):
+            print("语料库不存在")
+            return
+        dictionary = corpora.Dictionary.load_from_text(dict_file)
+
+    # 每一篇文档对应的稀疏向量（这里是bow向量）
+    corpus = [dictionary.doc2bow(doc) for doc in corpora_documents]
+
+    tfidf_model = models.TfidfModel(corpus)
+    '''
+    TF-IDF是一种统计方法，用以评估一字词对于一个文件集或一个语料库中的其中一份文件的重要程度。
+    字词的重要性随着它在文件中出现的次数成正比增加，但同时会随着它在语料库中出现的频率成反比下降
+    '''
+    corpus_tfidf = tfidf_model[corpus]
+
+    # 处理测试文本
+    text_corpus = dictionary.doc2bow(text)  # 转换成bow向量
+    text_corpus_tfidf = tfidf_model[text_corpus]  # 计算tfidf值
+
+    similarity_tfidf = similarities.Similarity(None, corpus_tfidf, num_features=600, num_best=5)
+    result1 = similarity_tfidf[text_corpus_tfidf]
+    print(result1)
+
+    lsi_model = models.LsiModel(corpus_tfidf)
+    corpus_lsi = lsi_model[corpus_tfidf]
+
+    similarity_lsi = similarities.Similarity(None, corpus_lsi, num_features=400, num_best=2)
+    text_corpus_lsi = lsi_model[text_corpus_tfidf]
+    result2 = similarity_lsi[text_corpus_lsi]
+    print(result2)
+
+
+def train(doc):
+    """
+    训练语料库
+    如果语料库文件不存在，则新增；否则追加当前文档到语料库
+    :param doc: 需要训练的文档
+    :return:
+    """
+    doc = [list(jieba.cut(doc))]
+    if os.path.exists(dict_file):
+        dictionary = corpora.Dictionary.load_from_text(dict_file)
+        dictionary.add_documents(doc)
+    else:
+        dictionary = corpora.Dictionary(doc)
+    dictionary.save_as_text(dict_file)
+
+
+def save_as_text(dictionary):
+    """
+    保存语料库
+    :param dictionary:
+    :return:
+    """
+    dictionary.save_as_text(dict_file)
+
+
+def add_documents(dictionary, documents, prune_at=None):
+    dictionary.add_documents(documents, prune_at)
+
+
+def cut(line):
+    return list(jieba.cut(line))
+
+
+if __name__ == '__main__':
+    raw_documents = [
+        '0南京江心洲污泥偷排”等污泥偷排或处置不当而造成的污染问题，不断被媒体曝光',
+        '1面对美国金融危机冲击与国内经济增速下滑形势，中国政府在2008年11月初快速推出“4万亿”投资十项措施',
+        '2全国大面积出现的雾霾，使解决我国环境质量恶化问题的紧迫性得到全社会的广泛关注',
+        '3大约是1962年的夏天吧，潘文突然出现在我们居住的安宁巷中，她旁边走着40号王孃孃家的大儿子，一看就知道，他们是一对恋人。那时候，潘文梳着一条长长的独辫',
+        '4坐落在美国科罗拉多州的小镇蒙特苏马有一座4200平方英尺(约合390平方米)的房子，该建筑外表上与普通民居毫无区别，但其内在构造却别有洞天',
+        '5据英国《每日邮报》报道，美国威斯康辛州的非营利组织“占领麦迪逊建筑公司”(OMBuild)在华盛顿和俄勒冈州打造了99平方英尺(约9平方米)的迷你房屋',
+        '6长沙市公安局官方微博@长沙警事发布消息称，3月14日上午10时15分许，长沙市开福区伍家岭沙湖桥菜市场内，两名摊贩因纠纷引发互殴，其中一人被对方砍死',
+        '7乌克兰克里米亚就留在乌克兰还是加入俄罗斯举行全民公投，全部选票的统计结果表明，96.6%的选民赞成克里米亚加入俄罗斯，但未获得乌克兰和国际社会的普遍承认',
+        '8京津冀的大气污染，造成了巨大的综合负面效应，显性的是空气污染、水质变差、交通拥堵、食品不安全等，隐性的是各种恶性疾病的患者增加，生存环境越来越差',
+        '9 1954年2月19日，苏联最高苏维埃主席团，在“兄弟的乌克兰与俄罗斯结盟300周年之际”通过决议，将俄罗斯联邦的克里米亚州，划归乌克兰加盟共和国',
+        '10北京市昌平区一航空训练基地，演练人员身穿训练服，从机舱逃生门滑降到地面',
+        '11腾讯入股京东的公告如期而至，与三周前的传闻吻合。毫无疑问，仅仅是传闻阶段的“联姻”，已经改变了京东赴美上市的舆论氛围',
+        '12国防部网站消息，3月8日凌晨，马来西亚航空公司MH370航班起飞后与地面失去联系，西安卫星测控中心在第一时间启动应急机制，配合地面搜救人员开展对失联航班的搜索救援行动',
+        '13新华社昆明3月2日电，记者从昆明市政府新闻办获悉，昆明“3·01”事件事发现场证据表明，这是一起由新疆分裂势力一手策划组织的严重暴力恐怖事件',
+        '14在即将召开的全国“两会”上，中国政府将提出2014年GDP增长7.5%左右、CPI通胀率控制在3.5%的目标',
+        '15中共中央总书记、国家主席、中央军委主席习近平看望出席全国政协十二届二次会议的委员并参加分组讨论时强调，团结稳定是福，分裂动乱是祸。全国各族人民都要珍惜民族大团结的政治局面，都要坚决反对一切危害各民族大团结的言行'
+    ]
+
+    for item_text in raw_documents:
+        train(item_text)
+    check(cut("南京江心洲污泥偷排”等污泥偷排或处置不当而造成的污染问题，不断被媒体曝光"))
